da8d3fd70ad11939f800a63fd812007493deba75
murakoshi <kennkyuu027@gmail.com>
2021-06-23 12:08:03 +0900

    token delete

--- app/src/main/java/de/tubs/ibr/dtn/sharebox/ui/SelectDestinationActivity.java
+++ app/src/main/java/de/tubs/ibr/dtn/sharebox/ui/SelectDestinationActivity.java
@@ -59,7 +59,8 @@ public class SelectDestinationActivity extends Activity {
     final int ASYNC_GET_ALL = 1;
     final int ASYNC_DELETE_ALL = 2;
     final int ASYNC_POST = 3;
-    final String SLACK_APP_TOKEN = "xoxb-1994031519219-2040722764727-AtVZyLvkrDrjmCc6lVPdRDn6";
+    //githubにアップロードする際にslack側で認証トークンが自動的に無効化されるためTOKENの中身を削除
+    final String SLACK_APP_TOKEN = "";
 
     private EditText enterfilename;
 
